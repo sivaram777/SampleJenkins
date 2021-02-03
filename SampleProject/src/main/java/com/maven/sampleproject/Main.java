2e9c294d87b0a06605ae935f0af2f2b8ef8c1376
srameshbabu7 <srameshbabu7@INPF0Y9BC8.CSCMWS.CSCMWS.COM>
2021-02-03 13:10:28 +0530

    first commit

---
+++ SampleProject/src/main/java/com/maven/sampleproject/Main.java
@@ -0,0 +1,22 @@
+package com.maven.sampleproject;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);      
+		System.out.print("Enter first number- ");  
+		int a= sc.nextInt();  
+		System.out.print("Enter second number- ");  
+		int b= sc.nextInt();  
+		int add=a+b; 
+		int sub=a-b;
+		int mul=a*b;
+		int div=a/b;
+		System.out.println("Addition= " +add);
+		System.out.println("Subtraction= " +sub);
+		System.out.println("Multiplication= " +mul);
+		System.out.println("Division= " +div);
+	}
+
+}
